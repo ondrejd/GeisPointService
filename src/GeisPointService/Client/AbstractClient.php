58c85ca823a0cbd8a33ae7ce5e86f7915b639153
ondrejd <ondrejd@gmail.com>
2015-09-01 20:24:15 +0200

    Initial version of client implementation.

---
+++ src/GeisPointService/Client/AbstractClient.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * GeisPointService - Library implementing PHP client for GeisPoint web service.
+ *
+ * @author Ondřej Doněk, <ondrej.donek@ebrana.cz>
+ * @license https://www.mozilla.org/MPL/2.0/ name
+ */
+
+namespace GeisPointClient;
+
+/**
+ * Abstract class for GeisPoint service clients.
+ *
+ * @author Ondřej Doněk, <ondrej.donek@ebrana.cz>
+ */
+abstract class AbstractClient
+{
+	/**
+	 * Client options.
+	 *
+	 * @param array $options
+	 */
+	protected $options;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param array $options
+	 * @return void
+	 */
+	public function __construct($options = array())
+	{
+		$this->options = $this->normalizeOptions($options);
+
+		// ...
+	}
+
+	/**
+	 * Normalizes array with service options.
+	 *
+	 * @param array $options
+	 * @return array
+	 */
+	protected function normalizeOptions(array $options)
+	{
+		if (!array_key_exists('useCache', $options)) {
+			$options['useCache'] = false;
+		}
+
+		// ...
+
+		return $options;
+	}
+
+	/**
+	 * Retrieve client options.
+	 *
+	 * @return array
+	 */
+	public function getOptions()
+	{
+		return $this->options;
+	}
+
+	/**
+	 * Select regions.
+	 *
+	 * @return void
+	 */
+	abstract public function selectRegionsAction();
+
+	/**
+	 * Select cities.
+	 *
+	 * @return void
+	 */
+	abstract public function selectCitiesAction();
+
+	/**
+	 * Search GeisPoint offices.
+	 *
+	 * @return void
+	 */
+	abstract public function searchPointsAction();
+
+	/**
+	 * Retrieve details about GeisPoint office.
+	 *
+	 * @return void
+	 */
+	abstract public function getPointDetailsAction();
+}
---
+++ src/GeisPointService/Client/Exception.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * GeisPointService - Library implementing PHP client for GeisPoint web service.
+ *
+ * @author Ondřej Doněk, <ondrej.donek@ebrana.cz>
+ * @license https://www.mozilla.org/MPL/2.0/ name
+ */
+
+namespace GeisPointService\Client;
+
+/**
+ * Simple exception class.
+ *
+ * @author Ondřej Doněk, <ondrej.donek@ebrana.cz>
+ */
+class Exception extends \Exception
+{
+	// ...
+}
